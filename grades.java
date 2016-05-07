d230e779cf18fd22482044b9608c7cbf1fa3e415
Justin Wilson <Justin.M.Wilson@hotmail.com>
2016-05-07 00:52:44 +0000

    Updated grades to allow for user input.

--- grades.java
+++ grades.java
@@ -1,33 +1,31 @@
 import java.util.*;
 
-public class hello
+public class grades
 {
 	public static void main(String[] args)
 	{
-
-		Random rand = new Random();
-
-		int numScores, n = 0;
+		int numScores = 0;
 		int total = 0;
 		double average;
-		//numScores++;
-		numScores = 10;
 
-		ArrayList<Integer> scores = new ArrayList<Integer>();
+		ArrayList<Integer> scores = new ArrayList<Integer>(); // to store the scores in
+		Scanner kb = new Scanner(System.in);
+		
+		//Prompt for input
+		System.out.println("Please enter grades.  If you are done entering grades, enter -1:");
+		int currentScore = Integer.parseInt(kb.nextLine()); 
+		//Takes whatever the user enters and parses it to an integer if possible.
 
-		// This following block is used just to populate the arraylist with example scores.
-		int temp;
-		while (n < numScores)
-		{	
-			temp = rand.nextInt();
-			if (temp > 0)
-			{
-				scores.add(temp % 101);
-				n++;
-			}
-		}
+        while (currentScore >= 0) // Keeps taking input until a value lower than 0 is entered
+        {
+            scores.add(currentScore);  // Adds the score to an arraylist
+            numScores++;  // Increments the total number of scores
+            currentScore = Integer.parseInt(kb.nextLine()); //Gets the next scroe from the user
+        }
+		
 
 		// This block prints out the passing scores
+		System.out.println("The following scores are passing:");
 		for(int i : scores)
 		{
 			if ( i >= 60 )
@@ -39,8 +37,11 @@ public class hello
 		}
 
 		// This divides the total "pool" by how many collected scores to generate the average
-		average = (double) total / numScores;
-
-		System.out.println("Average score is: " + average);
+		// so long as at least one score has been entered
+		if (numScores > 0)
+		{
+    		average = (double) total / numScores;
+	    	System.out.println("\nAverage score is: " + average);
+		}
 	}
 }
